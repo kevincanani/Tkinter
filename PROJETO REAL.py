a0bb1086b38e4790ff757e0f31dc82fca91ba197
kevincanani <kevin.232122@alunosatc.edu.br>
2024-11-12 17:15:21 -0300

    Update PROJETO REAL.py

--- PROJETO REAL.py
+++ PROJETO REAL.py
@@ -152,7 +152,7 @@ def Ppag():
             btnC.destroy()
             btnP.destroy()
 
-            Label3 = Label(menu,text="------------------------Cadastro do Cliente---------------------:",bg="Black",fg="White", font="Courier 10 bold")
+            Label3 = Label(menu,text="------------------------------------------------------------------------------------------------------------------------Cadastro do Cliente------------------------------------------------------------------------------------------------------------------------:",bg="Black",fg="White", font="Courier 10 bold")
             Label3.pack()
 
             id_label = Label(menu, text='ID: ', bg="White", font=('Courier'))
@@ -294,9 +294,6 @@ def Ppag():
  
         btnP = Button(menu, text="Projeto" , command=Pcad_pag,  width= 23,bg="Black",fg="White")
         btnP.place(x=865, y=175)
-
-    Label1 = Label(menu,text="------------------------------------------------------------------------------------------------------------------------Sistema de Gerenciamento UGC------------------------------------------------------------------------------------------------------------------------",bg="Black",fg="White", font="Courier 10 bold")
-    Label1.pack()
  
     def Altpag():
         def AltArq():
@@ -354,7 +351,7 @@ def Ppag():
             enderecoarq = Entry(menu, width=50,bg="Black",fg="White")
             enderecoarq.place(x=865, y=210)
 
-            btnconf = Button(menu, text="Confirmar",command=Volt, width= 23,bg="Black",fg="White")
+            btnconf = Button(menu, text="Confirmar",  width= 23,bg="Black",fg="White")
             btnconf.place(x=835, y=260)
 
             btnvolt = Button(menu, text="Voltar",command=Volt, width= 23,bg="Black",fg="White")
@@ -415,7 +412,7 @@ def Ppag():
             enderecocli = Entry(menu, width=50,bg="Black",fg="White")
             enderecocli.place(x=865, y=210)
 
-            btnconf = Button(menu, text="Confirmar",command=Volt, width= 23,bg="Black",fg="White")
+            btnconf = Button(menu, text="Confirmar", width= 23,bg="Black",fg="White")
             btnconf.place(x=835, y=260)
 
             btnvolt = Button(menu, text="Voltar",command=Volt, width= 23,bg="Black",fg="White")
@@ -476,17 +473,13 @@ def Ppag():
             DataF = Entry(menu, width=50,bg="Black",fg="White")
             DataF.place(x=865, y=210)
 
-            btnconf = Button(menu, text="Confirmar",command=Volt, width= 23,bg="Black",fg="White")
+            btnconf = Button(menu, text="Confirmar", width= 23,bg="Black",fg="White")
             btnconf.place(x=835, y=260)
 
             btnvolt = Button(menu, text="Voltar",command=Volt, width= 23,bg="Black",fg="White")
             btnvolt.place(x=1080, y=260)
 
-
-
-
-            
-
+    
 
         Label1.destroy()
         btn1.destroy()
@@ -507,7 +500,220 @@ def Ppag():
         btnP = Button(menu, text="Projeto" ,command=AltProj,  width= 23,bg="Black",fg="White")
         btnP.place(x=865, y=175)
 
+    def Pesq():
+        def PesqArq():
+            def Volt():
+                Label3.destroy()
+                telefone_labelarq.destroy()
+                telefonearq.destroy()
+                email_labelarq.destroy()
+                emailarq.destroy()
+                cidade_labelarq.destroy()
+                cidadearq.destroy()
+                estado_labelarq.destroy()
+                estadoarq.destroy()
+                endereco_labelarq.destroy()
+                enderecoarq.destroy()
+                btnconf.destroy()
+                btnvolt.destroy()
+                Ppag()
 
+            Label2.destroy()
+            btnA.destroy()
+            btnC.destroy()
+            btnP.destroy()
+
+            Label3 = Label(menu,text="------------------------------------------------------------------------------------------------------------------------Realizando Pesquisa------------------------------------------------------------------------------------------------------------------------",bg="Black",fg="White", font="Courier 10 bold")
+            Label3.pack()
+
+            telefone_labelarq = Label(menu, text='Telefone: ', bg="White", font=('Courier'))
+            telefone_labelarq.place(x=865, y=30)
+
+            telefonearq = Entry(menu, width=50,bg="Black",fg="White")
+            telefonearq.place(x=865, y=50)
+
+            email_labelarq = Label(menu, text='Email: ', bg="White", font=('Courier'))
+            email_labelarq.place(x=865, y=70)
+ 
+            emailarq = Entry(menu, width=50,bg="Black",fg="White")
+            emailarq.place(x=865, y=90)
+
+            cidade_labelarq = Label(menu, text='Cidade: ', bg="White", font=('Courier'))
+            cidade_labelarq.place(x=865, y=110)
+ 
+            cidadearq = Entry(menu, width=50,bg="Black",fg="White")
+            cidadearq.place(x=865, y=130)
+
+            estado_labelarq = Label(menu, text='Estado: ', bg="White", font=('Courier'))
+            estado_labelarq.place(x=865, y=150)
+ 
+            estadoarq = Entry(menu, width=50,bg="Black",fg="White")
+            estadoarq.place(x=865, y=170)
+
+            endereco_labelarq = Label(menu, text='Endereço: ', bg="White", font=('Courier'))
+            endereco_labelarq.place(x=865, y=190)
+ 
+            enderecoarq = Entry(menu, width=50,bg="Black",fg="White")
+            enderecoarq.place(x=865, y=210)
+
+            btnconf = Button(menu, text="Pesquisar", width= 23,bg="Black",fg="White")
+            btnconf.place(x=835, y=260)
+
+            btnvolt = Button(menu, text="Voltar",command=Volt, width= 23,bg="Black",fg="White")
+            btnvolt.place(x=1080, y=260)
+
+        def PesqCli():
+            def Volt():
+                Label3.destroy()
+                telefone_labelcli.destroy()
+                telefonecli.destroy()
+                email_labelcli.destroy()
+                emailcli.destroy()
+                cidade_labelcli.destroy()
+                cidadecli.destroy()
+                estado_labelcli.destroy()
+                estadocli.destroy()
+                endereco_labelcli.destroy()
+                enderecocli.destroy()
+                btnconf.destroy()
+                btnvolt.destroy()
+                Ppag()
+
+            Label2.destroy()
+            btnA.destroy()
+            btnC.destroy()
+            btnP.destroy()
+
+            Label3 = Label(menu,text="------------------------------------------------------------------------------------------------------------------------Realizando Pesquisa------------------------------------------------------------------------------------------------------------------------",bg="Black",fg="White", font="Courier 10 bold")
+            Label3.pack()
+
+            telefone_labelcli = Label(menu, text='Telefone: ', bg="White", font=('Courier'))
+            telefone_labelcli.place(x=865, y=30)
+
+            telefonecli = Entry(menu, width=50,bg="Black",fg="White")
+            telefonecli.place(x=865, y=50)
+
+            email_labelcli = Label(menu, text='Email: ', bg="White", font=('Courier'))
+            email_labelcli.place(x=865, y=70)
+ 
+            emailcli = Entry(menu, width=50,bg="Black",fg="White")
+            emailcli.place(x=865, y=90)
+
+            cidade_labelcli = Label(menu, text='Cidade: ', bg="White", font=('Courier'))
+            cidade_labelcli.place(x=865, y=110)
+ 
+            cidadecli = Entry(menu, width=50,bg="Black",fg="White")
+            cidadecli.place(x=865, y=130)
+
+            estado_labelcli = Label(menu, text='Estado: ', bg="White", font=('Courier'))
+            estado_labelcli.place(x=865, y=150)
+ 
+            estadocli = Entry(menu, width=50,bg="Black",fg="White")
+            estadocli.place(x=865, y=170)
+
+            endereco_labelcli = Label(menu, text='Endereço: ', bg="White", font=('Courier'))
+            endereco_labelcli.place(x=865, y=190)
+ 
+            enderecocli = Entry(menu, width=50,bg="Black",fg="White")
+            enderecocli.place(x=865, y=210)
+
+            btnconf = Button(menu, text="Pesquisar", width= 23,bg="Black",fg="White")
+            btnconf.place(x=835, y=260)
+
+            btnvolt = Button(menu, text="Voltar",command=Volt, width= 23,bg="Black",fg="White")
+            btnvolt.place(x=1080, y=260)
+
+        def PesqProj():
+            def Volt():
+                Label3.destroy()
+                nome_label.destroy()
+                nome.destroy()
+                tipo_label.destroy()
+                tipo.destroy()
+                orcamento_label.destroy()
+                orcamento.destroy()
+                data_inicio_label.destroy()
+                data_inicio.destroy()
+                data_entrega_label.destroy()
+                data_entrega.destroy()
+                btnConfirmar.destroy()
+                btnVoltar.destroy()
+                Ppag()
+
+            Label2.destroy()
+            btnA.destroy()
+            btnC.destroy()
+            btnP.destroy()
+
+            Label3 = Label(menu,text="------------------------------------------------------------------------------------------------------------------------Realizando Pesquisa------------------------------------------------------------------------------------------------------------------------",bg="Black",fg="White", font="Courier 10 bold")
+            Label3.pack()
+
+            nome_label = Label(menu, text='Nome: ', bg="White", font=('Courier'))
+            nome_label.place(x=865, y=75)
+
+            nome = Entry(menu, width=50, background="Black", foreground="White")
+            nome.place(x=865, y=105)
+
+            tipo_label = Label(menu, text='Tipo: ', bg="White", font=('Courier'))
+            tipo_label.place(x=865, y=135)
+
+            tipo = Entry(menu, width=50, background="Black", foreground="White")
+            tipo.place(x=865, y=165)
+
+            orcamento_label = Label(menu, text='Orçamento: ', bg="White", font=('Courier'))
+            orcamento_label.place(x=865, y=195)
+
+            orcamento = Entry(menu, width=50, background="Black", foreground="White")
+            orcamento.place(x=865, y=225)
+
+            data_inicio_label = Label(menu, text='Data de início: ', bg="White", font=('Courier'))
+            data_inicio_label.place(x=865, y=255)
+
+            data_inicio = Entry(menu, width=50, background="Black", foreground="White")
+            data_inicio.place(x=865, y=285)
+
+            data_entrega_label = Label(menu, text='Data de entrega: ', bg="White", font=('Courier'))
+            data_entrega_label.place(x=865, y=315)
+
+            data_entrega = Entry(menu, width=50, background="Black", foreground="White")
+            data_entrega.place(x=865, y=345)
+
+            btnConfirmar = Button(menu, text="Confirmar", width=15,bg="Black",fg="White")
+            btnConfirmar.place(x=835, y=645)
+
+            btnVoltar = Button(menu, text="Voltar", command=Volt, width=15,bg="Black",fg="White")
+            btnVoltar.place(x=1080, y=260)
+            
+
+            
+
+
+
+
+        Label1.destroy()
+        btn1.destroy()
+        btn2.destroy()
+        btn3.destroy()
+        btn4.destroy()
+        btn5.destroy()
+        
+        Label2 = Label(menu,text="------------------------------------------------------------------------------------------------------------------------Escolha a opção desejada para Pesquisar------------------------------------------------------------------------------------------------------------------------:",bg="Black",fg="White", font="Courier 10 bold")
+        Label2.pack()
+    
+        btnA = Button(menu, text="Arquiteto" ,command=PesqArq,  width= 23,bg="Black",fg="White")
+        btnA.place(x=865, y=75)
+        
+        btnC = Button(menu, text="Cliente" ,command=PesqCli,  width= 23,bg="Black",fg="White")
+        btnC.place(x=865, y=125)
+        
+        btnP = Button(menu, text="Projeto" ,command=PesqProj,  width= 23,bg="Black",fg="White")
+        btnP.place(x=865, y=175)
+
+        
+
+
+    Label1 = Label(menu,text="------------------------------------------------------------------------------------------------------------------------Sistema de Gerenciamento UGC------------------------------------------------------------------------------------------------------------------------",bg="Black",fg="White", font="Courier 10 bold")
+    Label1.pack()
 
     #botao C
     btn1 = Button(menu, text="Cadastrar" , command=Spag,  width= 23,bg="Black",fg="White")
@@ -522,7 +728,7 @@ def Ppag():
     btn3.place(x=865, y=175)
  
     #botao P
-    btn4 = Button(menu, text="Pesquisar" , width= 23,bg="Black",fg="White")
+    btn4 = Button(menu, text="Pesquisar" ,command=Pesq, width= 23,bg="Black",fg="White")
     btn4.place(x=865, y=225)
  
     #botao S
